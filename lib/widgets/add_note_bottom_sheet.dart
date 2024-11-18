a8fcbd9d7fc90a4fb77f47e090e5031d04de9809
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2024-11-18 06:38:49 +0200

    constants

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottom extends StatelessWidget {
   const AddNoteBottom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 16,
+          ),
+          Text('Add New Note', style: TextStyle(fontSize: 24)),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(),
+          SizedBox(height: 20),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
